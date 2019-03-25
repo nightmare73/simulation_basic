d65fa5296053aeff1b5238701c1b2a34a37b3515
Malibin <nightmare73@naver.com>
2019-03-25 14:22:27 +0900

    homework 1-1문제 코드작성완료

---
+++ HomeWork1/Problem_1_1.m
@@ -0,0 +1,36 @@
+% n = 원 위의 균등한 거리로 떨어져있는 점의 갯수
+n = 3;
+% An = n개의 점을 이은 원 내부 다각형 넓이
+An = A(n);
+% Bn = n개의 점들을 각각 접선으로 하는 원 외부의 다각형 넓이
+Bn = B(n);
+
+Pn = (An + Bn) / 2;
+% to = 절대오차
+to = abs(Pn - pi);
+
+%tol = 절대 오차 임계값
+tol = input('절대 오차 임계값 입력 : ');
+
+while(~(to < tol))
+     n = n + 1;
+    An = A(n);
+    Bn = B(n);
+    
+    Pn = (An + Bn) / 2;
+    to = abs(Pn - pi);
+end
+
+disp('가장 작은 정수 n : ')
+disp(n);
+disp('이 때의 절대 오차값 : ');
+disp(to);
+
+% n개의 점을 이은 원 내부 다각형 넓이
+function f = A(n)
+f = n / 2 * sin(2 * pi / n);
+end
+% n개의 점들을 각각 접선으로 하는 원 외부의 다각형 넓이
+function f = B(n)
+f = n * tan(pi / n);
+end
